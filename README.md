1fb12cbb6b4293359793176fc5a978ae73eb3d7d
Farruh <ufarruh@gmail.com>
2016-10-13 20:03:52 -0700

    User table

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ viking_store
 ============
 
 The Viking Store e-commerce application for Viking Commerce Inc
+
+## by Farruh
+
+## Data Models
+
+### Users
+
+1 id
+2 first_name, required field, string
+3 last_name, required field, string
+4 email, valid format, required field, string
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+User.destroy_all
+
+100.times do |num|
+  u = User.new
+  u.first_name = "#{Faker::Name.first_name}"
+  u.last_name = "#{Faker::Name.last_name}"
+  u.save!
+end
